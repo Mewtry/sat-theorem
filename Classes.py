d07e40cee1282aefae5172831cf9c99e392ea809
Mewtry <theo.v.p.com@gmail.com>
2021-09-27 12:29:55 -0300

    Eliminando a classe Funcs e distribuindo seus métodos, denovo. Terminei de acrescentar as funções de vetores e pontos. Primeira parte do teste já está funcionando (AABB).

--- Classes.py
+++ Classes.py
@@ -1,4 +1,6 @@
-from math import pi
+from typing import *
+from math import sqrt, acos, pi
+
 
 class Ponto2D:
     def __init__(self, coord):
@@ -14,13 +16,23 @@ class Ponto2D:
     def get_par(self):
         return self.x, self.y
 
-    def soma_vetor_ao_ponto(self, a, b):
-        self.x = a.x + b.x
-        self.y = a.y + b.y
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
 
     def subtrai_vetor_do_ponto(self, a, b):
+        if not (isinstance(a, Ponto2D) and isinstance(b, Vetor2D)):
+            raise TypeError
         self.x = a.x - b.x
         self.y = a.y - b.y
+
+    def soma_vetor_ao_ponto(self, a, b):
+        if not (isinstance(a, Ponto2D) and isinstance(b, Vetor2D)):
+            raise TypeError
+        self.x = a.x + b.x
+        self.y = a.y + b.y
 
     def __str__(self):
         return str(self.get_par())
@@ -40,24 +52,55 @@ class Vetor2D:
         self.x = a.x - b.x
         self.y = a.y - b.y
 
+    def ret_diferenca_entre_pontos(self, a: Ponto2D):
+        return Vetor2D(self.x - a.x, self.y - a.y)
+
     def soma_vetores(self, a, b):
+        if not (isinstance(a, Vetor2D) and isinstance(b, Vetor2D)):
+            raise TypeError
         self.x = a.x + b.x
         self.y = a.y + b.y
 
     def subtrai_vetores(self, a, b):
+        if not (isinstance(a, Vetor2D) and isinstance(b, Vetor2D)):
+            raise TypeError
         self.x = a.x - b.x
         self.y = a.y - b.y
+
+    def produto_escalar_ret_orig(self, escalar):
+        self.x *= escalar
+        self.y *= escalar
+
+    def dot_product(self, a):
+        return self.x * a.x + self.y * a.y
+
+    def comprimento_vetor(self):
+        return sqrt(pow(self.x, 2) + pow(self.y, 2))
+
+    def normaliza_vetor(self):
+        c = self.comprimento_vetor()
+        self.x /= c
+        self.y /= c
+
+    def vetor_direcao(self):
+        self.normaliza_vetor()
+        if self.y > 0:
+            return acos(self.x)
+        return acos(self.x) + pi
 
     def __str__(self):
         return str(self.get_par())
 
 
 class Matriz2x2:
-    def __init__(self, m):
-        self.elementos = m
+    def __init__(self, m: List):
+        self.elementos = [m[:2], m[2:]]
+
+    def __str__(self):
+        return f'Matriz: {self.elementos[0]}\n        {self.elementos[1]}'
 
 
-class Poligonos:
+class Poligono:
     def __init__(self, org_dist, num_vert, vertices):
         self.org_dist = Vetor2D(org_dist[0], org_dist[1])
         self.num_vert = num_vert
@@ -69,13 +112,11 @@ class Poligonos:
         return self.num_vert
 
     def get_vertices(self):
-        pontos = []
         lista = []
         for i in range(self.num_vert):
-            pontos.append(self.vertices[i].x)
-            pontos.append(self.vertices[i].y)
+            pontos = [self.vertices[i].x, self.vertices[i].y]
             lista.append(pontos)
-            pontos = []
+
         return lista
 
     def get_one_vertice(self, index):
@@ -90,18 +131,66 @@ class Poligonos:
     def plot(self, tela: tuple):
         pass
 
+    def check_colisao(self, obj):
+        # Primeiro teste de intersecção de Axis Aligned Bounding Boxes. (AABB)
+        min_a = Ponto2D([self.get_one_vertice(0).x, self.get_one_vertice(0).y])
+        max_a = Ponto2D([self.get_one_vertice(0).x, self.get_one_vertice(0).y])
+        for i in range(1, self.get_num_vert()):
+            if self.get_one_vertice(i).x < min_a.x:
+                min_a.set_x(self.get_one_vertice(i).x)
+            if self.get_one_vertice(i).y < min_a.y:
+                min_a.set_y(self.get_one_vertice(i).y)
+
+            if self.get_one_vertice(i).x > max_a.x:
+                max_a.set_x(self.get_one_vertice(i).x)
+            if self.get_one_vertice(i).y > max_a.y:
+                max_a.set_y(self.get_one_vertice(i).y)
+
+        min_b = Ponto2D([obj.get_one_vertice(0).x, obj.get_one_vertice(0).y])
+        max_b = Ponto2D([obj.get_one_vertice(0).x, obj.get_one_vertice(0).y])
+        for i in range(1, obj.get_num_vert()):
+            if obj.get_one_vertice(i).x < min_b.x:
+                min_b.set_x(self.get_one_vertice(i).x)
+            if obj.get_one_vertice(i).y < min_b.y:
+                min_b.set_y(self.get_one_vertice(i).y)
+
+            if obj.get_one_vertice(i).x > max_b.x:
+                max_b.set_x(self.get_one_vertice(i).x)
+            if obj.get_one_vertice(i).y > max_b.y:
+                max_b.set_y(self.get_one_vertice(i).y)
+
+        if min_a.x < max_b.x and min_b.x < max_a.x and min_a.y < max_b.y and min_b.y < max_a.y:
+
+            print('Há a possibilidade de estar colidindo')
+            print(min_a, max_a, min_b, max_b)
+
+        else:
+            print('Não está colidindo')
+            print(min_a, max_a, min_b, max_b)
+
     def __str__(self):
         return 'org_dist: ' + str(self.org_dist.get_par()) + \
                '\nNumero de vertices: ' + str(self.num_vert) + \
                '\nVertices: ' + str(self.get_vertices())
 
 
-# ----------------- Teste da classe ------------------
-'''tupladetuplas = [(4, 5), (5, 7), (8, 9), (10, 11), (12, 13)]
-obj = Poligonos([1, 2], len(tupladetuplas), tupladetuplas)
-a = Vetor2D(4, 4)
-b = Vetor2D(1, 0)
-v = Vetor2D(0, 0)
-v.soma_vetores(a, b)
-print(v)
-print(obj)'''
+# ----------------- Teste das classes ------------------
+tupladetuplas = ((0, 0), (0, 2), (2, 2), (2, 0))
+teste1 = Poligono([1, 1], len(tupladetuplas), tupladetuplas)
+print(teste1)
+
+vert = ((2, 0), (2, 2), (4, 2), (4, 0))
+teste2 = Poligono([2, 1], len(vert), vert)
+print(teste2)
+
+teste1.check_colisao(teste2)
+
+
+
+# m = Matriz2x2([1, 2, 3, 4])
+# print(m)
+'''
+v= Vetor2D(0, 0)
+a = Ponto2D([4, 4])
+b = Ponto2D([1, 1])
+print(v)'''
